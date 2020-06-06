8a3e8f67105406ab17fb6b043488a93e0a0c552a
pedrodalvy <pedrodalvy@outlook.com>
2020-06-06 19:01:13 -0400

    Create FormTaskList.vue

---
+++ resources/js/components/tasks/FormTaskList.vue
@@ -0,0 +1,47 @@
+<template>
+    <form @submit.prevent="addTaskList" class="form-inline mx-4 mb-4" id="form_new_task">
+        <div class="form-group col-md-6 px-0">
+            <input class="form-control"
+                   id="name" placeholder="Nova Lista de Tarefas"
+                   type="text" v-model="taskList.name">
+        </div>
+        <button class="btn btn-primary mx-2" type="submit">Adicionar</button>
+
+        <button @click="hideForm"
+                class="btn btn-danger mx-2">
+            Cancelar
+        </button>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "FormTaskList",
+        data() {
+            return {
+                taskList: { name: '' },
+                showForm: true
+            }
+        },
+        methods: {
+            addTaskList() {
+                this.$emit('createTaskList', this.taskList);
+                this.taskList = { name: '' };
+            },
+            hideForm() {
+                this.$emit('hideForm', !this.showForm);
+            }
+        },
+    }
+
+</script>
+
+<style scoped>
+    #name {
+        width: 100%;
+    }
+
+    #form_new_task {
+        width: 100%;
+    }
+</style>
