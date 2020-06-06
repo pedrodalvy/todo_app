1f849e921dff53dc6dff968272e30ca5c398d37a
pedrodalvy <pedrodalvy@outlook.com>
2020-06-06 15:58:19 -0400

    Criado interceptor para redirecionamento de usuario nao autenticado

--- resources/js/services/resources.js
+++ resources/js/services/resources.js
@@ -2,13 +2,31 @@ import 'vue-resource';
 import CONFIG from './config';
 import Vue from 'vue';
 import JwtToken from './jwt-token.js';
+import store from '../store/store';
+import router from '../router';
 
 Vue.http.options.root = CONFIG.API_URL;
+
 Vue.http.interceptors.push((request, next) => {
     if (JwtToken.token) {
         request.headers.set('Authorization', JwtToken.getAuthorizationHeader());
     }
     next();
+});
+
+Vue.http.interceptors.push((request, next) => {
+    next( response => {
+        let authorization = response.headers.get('Authorization');
+        if (authorization) {
+            JwtToken.token = authorization.split(' ')[1];
+        }
+        switch (response.status) {
+            case 401:
+                JwtToken.token = null;
+                store.commit('auth/unauthenticated');
+                return router.push('login');
+        }
+    })
 });
 
 export class Jwt {
